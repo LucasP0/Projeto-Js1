4d31c4c809684d622fea8d6b8449b9b72916092a
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-02-28 18:58:48 -0300

    Depressão diaria

---
+++ classes/class01/pages/video-player/src/factory.js
@@ -0,0 +1,23 @@
+import Controller from "./controller.js";
+import Service from "./service.js";
+import View from "./view.js";
+import Camera from "../../../lib/shared/camera";
+
+if (supportsWorkerType()) {
+  console.log('Suporta')
+} {
+  console.log('Não suporta')
+}
+
+const camera = await Camera.init()
+const [rootPath] = window.location.href.split('/pages/')
+const factory = {
+  async initalize() {
+    return Controller.initialize({
+      view: new View({}),
+      service: new Service({})
+    })
+  }
+}
+
+export default factory
