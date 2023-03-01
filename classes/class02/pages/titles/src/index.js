b44d212da8ff8f856a9fda0c0cbfe5411fca60a2
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-03-01 16:44:45 -0300

    Atualização

--- classes/class02/pages/titles/src/index.js
+++ classes/class02/pages/titles/src/index.js
@@ -1,3 +1,5 @@
 import cardsFactory from "./factories/cardsFactory.js"
+import handGestureFactory from "../src/factories/handGestureFactory.js"
 
 await cardsFactory.initialize()
+await handGestureFactory.initialize()
