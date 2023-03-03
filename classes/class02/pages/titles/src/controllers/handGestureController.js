0fc95407458178de1ece93d52fdf29a6f8dfed4f
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-03-03 01:13:05 -0300

    Update moonstrãoooo

--- classes/class02/pages/titles/src/controllers/handGestureController.js
+++ classes/class02/pages/titles/src/controllers/handGestureController.js
@@ -1,12 +1,30 @@
 export default class HandGestureController {
   #view
   #service
-  constructor({ view, service }) {
+  #camera
+  constructor({ view, service, camera }) {
     this.#service = service
     this.#view = view
+    this.#camera = camera
   }
-  async init() {}
+  async init() {
+    return this.#loop()
+  }
 
+  async #estimateHands() {
+    try {
+      const hands = await this.#service.estimateHands(this.#camera.video)
+      console.log({ hands })
+    } catch (error) {
+      console.error('Ferrou Meno', error)
+    } 
+  }
+
+  async #loop() {
+    await this.#service.initializeDetector()
+    await this.#estimateHands()
+    this.#view.loop(this.#loop.bind(this))
+  } 
   static async initialize(deps) {
     const controller = new HandGestureController(deps)
     return controller.init()
--- classes/class02/pages/titles/src/services/handGestureService.js
+++ classes/class02/pages/titles/src/services/handGestureService.js
@@ -1,1 +1,57 @@
-export default class HandGestureService {}
+import { knowGestures, gestureStrings } from "../util/gestures.js"
+export default class HandGestureService {
+  #gestureEstimator
+  #handPoseDetection
+  #handsVersion
+  #detector = null
+
+  constructor({ fingerpose, handPoseDetection, handsVersion }) {
+    this.#gestureEstimator = new fingerpose.GestureEstimator(knowGestures) 
+    this.#handPoseDetection = handPoseDetection
+    this.#handsVersion = handsVersion
+  }
+
+  async estimate(keypoints3D) {
+    const predictions = await this.#gestureEstimator.estimate(
+      this.#getLandMarksFromKeypoints(keypoints3D),
+      // porcentagem de confiaça
+      9
+    )
+    console.log({ predictions })
+  }
+
+  async * detectGestures(predictions) {
+    for(const hand of predictions) {
+      const gestures = await this.estimate(hand.keypoints3D)
+    }
+  }
+
+  #getLandMarksFromKeypoints(keypoints3D) {
+    return keypoints3D.map(keypoint => 
+      [keypoint.x, keypoint.y, keypoint.z])
+  }
+  
+  async estimateHands(video) {
+    return this.#detector.estimateHands(video, {
+      flipHorizontal: true
+    })
+  }
+
+  async initializeDetector() {
+    if(this.#detector) return this.#detector
+
+    const detectorConfig = {
+      runtime: 'mediapipe', // or 'tfjs',
+      solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${this.#handsVersion}`,
+      // full é o mais pesado e o mais preciso
+      modelType: 'lite',
+      maxHands: 2,
+    }
+    this.#detector = await this.#handPoseDetection.createDetector(
+      this.#handPoseDetection.SupportedModels.MediaPipeHands, 
+      detectorConfig
+    )
+
+    return this.#detector
+  }
+} 
